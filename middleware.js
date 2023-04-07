596218bad76db4e8be1d17398d0e65754155b52a
trinhnguyenx <nguyencongtrinhqb@gmail.com>
2023-04-07 20:45:30 +0700

    sss

--- middleware.js
+++ middleware.js
@@ -1,9 +1,12 @@
 function validate(req, res, next) {
-    const { fullname, age } = req.body;
-    if (/^[^0-9]+$/.test(fullname) && age >= 0) {
-      next();
-    } else {
-      res.status(400).send({ message: 'Invalid data' });
-    }
+  let checknumber = /^[^0-9]+$/;
+  let checkstr = /[^\w\s]/g;
+  const fullname = req.body && req.body.fullname;
+  const age = req.body && req.body.age;
+  if (checknumber.test(fullname) && checkstr.test(fullname) && age >= 0) {
+    next();
+  } else {
+    res.status(400).send({ message: "Invalid data" });
   }
+}
 module.exports = validate;
