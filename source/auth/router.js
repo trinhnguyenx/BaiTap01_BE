74194ca751c1744796e6b297fbe6dc86140a5d1f
trinhnguyenx <nguyencongtrinhqb@gmail.com>
2023-05-05 00:35:52 +0700

    done changed MVC

--- source/auth/router.js
+++ source/auth/router.js
@@ -1,131 +1,14 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const crypto = require("crypto");
 const app = express();
 
 const db = require("../database/connection");
-const { hashPassword } = require("../auth/flowhash");
-app.get("/", (req, res) => {
-  console.log("aloalo");
-});
-app.post("/register", async function (req, res) {
-  //get data from request body
-  const { username, password, name, age, email, gender } = req.body;
+const UserController = require("./controller");
+app.get("/", (req, res) => {});
 
-  db.query(`SELECT * FROM user WHERE username = ?`, username, (err, rows) => {
-    if (err) {
-      return res.status(500).json({
-        message: "Internal server error",
-      });
-    }
+app.post("/register", UserController.registerUser);
 
-    const user = rows[0];
+app.post("/login", UserController.loginUser);
 
-    if (user) {
-      return res.status(400).json({
-        message: "User already taken",
-      });
-    }
-  });
-
-  const { hashedPassword, salt } = hashPassword(password);
-  db.query(
-    "INSERT INTO user (username, name,salt , password,age,email,gender) VALUES (?,?,?,?,?,?,?)",
-    [username, name, salt, hashedPassword, age, email, gender],
-    (err, rows) => {
-      if (err) {
-        return res.status(500).json({ message: "Internal sever error" });
-      }
-      return res.status(201).json({ message: "Register successfully" });
-    }
-  );
-});
-
-//POST
-
-app.post("/login", async function (req, res) {
-  const { usernamelg, passwordlg } = req.body;
-  if (!usernamelg || !passwordlg) {
-    return res
-      .status(400)
-      .json({ message: "Username and password are required!" });
-  }
-
-  try {
-    db.query(
-      `SELECT * FROM user WHERE username = ?`,
-      usernamelg,
-      async (err, results) => {
-        if (err) {
-          return res.status(500).json({ message: "Internal sever error !" });
-        }
-        if (results.length === 0) {
-          return res
-            .status(404)
-            .json({ message: "Invalid username or password!" });
-        }
-        const user = results[0];
-        // Compare password hashes
-        const hashedPasswordlg = crypto
-          .pbkdf2Sync(passwordlg, user.salt, 1000, 64, "sha1")
-          .toString("hex");
-        if (hashedPasswordlg !== user.password) {
-          // console.log(user.salt);
-          // console.log(hashedPasswordlg);
-          return res
-            .status(401)
-            .json({ message: "Invalid username or password!1" });
-        }
-        // Sign JWT token
-        const token = jwt.sign({ id: user.ID_user }, process.env.JWT_SECRET, {
-          algorithm: "HS256",
-          expiresIn: "24h",
-        });
-        // console.log(user.ID_user);
-        // Send response with token
-        res.json({ token });
-      }
-    );
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-//PUT
-
-// Route for updating user info
-app.put("/user/:id", async function (req, res) {
-  const { name, age, gender } = req.body;
-  const userId = req.params.id;
-
-  // Validate request
-  if (!name && !age && !gender) {
-    return res.status(400).json({ message: "At least one field is required!" });
-  }
-
-  try {
-    // Verify token
-    const token = req.headers.authorization.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    if (decoded.id === userId) {
-      return res.status(401).json({ message: "Unauthorized!" });
-    }
-
-    db.query(
-      `UPDATE user SET name = ?, gender = ?, age = ? where ID_user = ?`,
-      [req.body.name, req.body.gender, req.body.age, req.params.id],
-      (err, results) => {
-        if (err) {
-          return res.status(500).json({ message: "Internal sever error !" });
-        }
-
-        // Response
-        res.json({ message: "User info updated successfully!" });
-      }
-    );
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+app.put("/user/:id", UserController.updateUser);
 
 module.exports = app;
