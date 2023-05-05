d65e90f5325d58e9dafed5a003f0546f39089147
trinhnguyenx <nguyencongtrinhqb@gmail.com>
2023-05-05 20:00:48 +0700

    done changed to MVC

--- source/auth/controller.js
+++ source/auth/controller.js
@@ -1,4 +1,5 @@
 const UserService = require("./service");
+const jwt = require("jsonwebtoken");
 
 class UserController {
   static async registerUser(req, res) {
@@ -33,10 +34,22 @@ class UserController {
   static async updateUser(req, res) {
     const userId = req.params.id;
     const { name, age, gender } = req.body;
-
+    // Validate request
+    if (!name && !age && !gender) {
+      return res
+        .status(400)
+        .json({ message: "At least one field is required!" });
+    }
     try {
+      // Verify token
+      const token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      if (decoded.id != userId) {
+        return res.status(401).json({ message: "Unauthorized!" });
+      }
+
       const message = await UserService.updateUser(userId, name, age, gender);
-      return res.status(200).json({ message });
+      return res.status(200).json({ message: "update infor successfully!" });
     } catch (error) {
       return res.status(500).json({ error: error.message });
     }
